55fa61af308351bc8ec3343925a22a26f5369104
Soumyabrata Sarkar <sarkar.soumyabrata2@gmail.com>
2025-10-22 22:27:44 +0530

    Added jokes show page

--- src/componenets/JokeBox.jsx
+++ src/componenets/JokeBox.jsx
@@ -1,5 +1,14 @@
-const JokeBox = () => {
-  return <div>JokeBox</div>;
+const JokeBox = ({ joke }) => {
+  return (
+    <div className="flex flex-col justify-between rounded-md border shadow-sm p-3 w-full md:w-[300px]">
+      <div className="flex flex-col justify-center items-center text-5xl h-[70px]">
+        🤭
+      </div>
+      <div className="text-center p-7">
+        <span>{joke}</span>
+      </div>
+    </div>
+  );
 };
 
 export default JokeBox;
--- src/pages/JokesPage.jsx
+++ src/pages/JokesPage.jsx
@@ -1,7 +1,28 @@
+import JokeBox from "../componenets/JokeBox";
+
 const JokesPage = () => {
+  const funnyJokesLines = [
+    "Freshly brewed jokes, just for you ☕😂",
+    "Warning: Jokes incoming! Handle with laughter.",
+    "Certified giggles — tailor-made for you!",
+    "Jokes for you… because someone had to laugh today.",
+    "Comedy delivery service: one laugh at a time.",
+    "I brought jokes. You bring the laugh.",
+    "Jokes for you, straight from my giggle factory 🎭",
+    "Knock knock — it’s me, bringing jokes again 😜",
+  ];
+
+  const getLine = () => {
+    return funnyJokesLines[Math.floor(Math.random() * funnyJokesLines.length)];
+  };
+
   return (
-    <div className="flex flex-col justify-center items-center h-[70vh]">
-      JokesPage
+    <div className="flex flex-col justify-center items-center h-[70vh] gap-12">
+      <span className="text-xl md:text-3xl">{getLine()}</span>
+      <div className="flex flex-col gap-3 md:flex-row">
+        <JokeBox joke={"some joke"} />
+        <JokeBox joke={"some joke"} />
+      </div>
     </div>
   );
 };
